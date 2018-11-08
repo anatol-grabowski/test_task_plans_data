8a08964e53f270cbb64fcd1155658d75a51981b0
tota <grabantot@gmail.com>
2018-11-08 18:38:47 +0300

    moved notebooks

--- migrations-flow/config/webpack.config.prod.js
+++ migrations-flow/config/webpack.config.prod.js
@@ -3,7 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
-const prodBaseUrl = 'migrations-flow/dist/'
+const prodBaseUrl = '../migrations-flow/dist/'
 
 const config = {
   ...commonConfig,
@@ -20,6 +20,6 @@ config.output.publicPath = prodBaseUrl // added to script tags in html
 const htmlWpPlugin = config.plugins.find(pl => pl.constructor === HtmlWebpackPlugin)
 htmlWpPlugin.options.baseUrl = prodBaseUrl // accessed in html through <%= htmlWebpackPlugin.options.baseUrl %>
 const definePlugin = config.plugins.find(pl => pl.constructor === webpack.DefinePlugin)
-definePlugin.definitions['DATA_PATH'] = JSON.stringify('outputs/') // accessed in js
+definePlugin.definitions['DATA_PATH'] = JSON.stringify('../outputs/') // accessed in js
 
 module.exports = config
